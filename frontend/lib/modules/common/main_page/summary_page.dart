5752f1a024d6a1c36623b6ed0a1b33f8fddfb9e6
WL <limweilun3838@gmail.com>
2025-05-17 11:12:11 +0800

    Add file

---
+++ frontend/lib/modules/common/main_page/summary_page.dart
@@ -0,0 +1,29 @@
+import 'package:Finance/core/widget/k_app_bar.dart';
+import 'package:Finance/core/widget/k_page.dart';
+import 'package:flutter/material.dart';
+
+class SummaryPage extends StatelessWidget {
+  const SummaryPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return KPage(
+      child: Column(
+        children: [
+          Expanded(
+            child: Container(
+              decoration: BoxDecoration(color: Colors.red),              
+            ),
+          ),
+          Container(
+            decoration: BoxDecoration(
+              color: Colors.white, 
+              borderRadius: BorderRadius.circular(50)
+            ),
+            child: Text("data"),
+          )
+        ],
+      ),
+    );
+  }
+}
